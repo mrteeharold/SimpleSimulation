4d3f10873859cdbd5d519ecad8a6c935ed860a21
ethung <e.h.h.thung@tudelft.nl>
2023-12-28 14:56:34 +0100

    multi playerwq

---
+++ monitor-teams/MonitorTeams.cxx
@@ -0,0 +1,242 @@
+/* ------------------------------------------------------------------   */
+/*      item            : MonitorTeams.cxx
+        made by         : ethung
+        from template   : DuecaModuleTemplate.cxx (2022.06)
+        date            : Fri Dec 22 17:33:15 2023
+        category        : body file
+        description     :
+        changes         : Fri Dec 22 17:33:15 2023 first version
+        language        : C++
+        copyright       : (c)
+*/
+
+#define MonitorTeams_cxx
+
+// include the definition of the module class
+#include "MonitorTeams.hxx"
+
+// include additional files needed for your calculation here
+
+// the standard package for DUSIME, including template source
+#define DO_INSTANTIATE
+#include <dueca.h>
+// #include <dueca/inter/ReplicatorInfo.hxx>
+
+// include the debug writing header, by default, write warning and
+// error messages
+#define W_MOD
+#define E_MOD
+#include <debug.h>
+
+// class/module name
+const char* const MonitorTeams::classname = "monitor-teams";
+
+// Parameters to be inserted
+const ParameterTable* MonitorTeams::getMyParameterTable()
+{
+  static const ParameterTable parameter_table[] = {
+    { "set-timing",
+      new MemberCall<_ThisModule_,TimeSpec>
+        (&_ThisModule_::setTimeSpec), set_timing_description },
+
+    { "check-timing",
+      new MemberCall<_ThisModule_,std::vector<int> >
+      (&_ThisModule_::checkTiming), check_timing_description },
+
+    /* You can extend this table with labels and MemberCall or
+       VarProbe pointers to perform calls or insert values into your
+       class objects. Please also add a description (c-style string).
+
+       Note that for efficiency, set_timing_description and
+       check_timing_description are pointers to pre-defined strings,
+       you can simply enter the descriptive strings in the table. */
+
+    /* The table is closed off with NULL pointers for the variable
+       name and MemberCall/VarProbe object. The description is used to
+       give an overall description of the module. */
+    { NULL, NULL, "please give a description of this module"} };
+
+  return parameter_table;
+}
+
+// constructor
+MonitorTeams::MonitorTeams(Entity* e, const char* part, const
+                   PrioritySpec& ps) :
+  /* The following line initialises the SimulationModule base class.
+     You always pass the pointer to the entity, give the classname and the
+     part arguments. */
+  Module(e, classname, part),
+
+  // initialize the data you need in your simulation or process
+
+  // initialize the channel access tokens, check the documentation for the
+  // various parameters. Some examples:
+  // r_mytoken(getId(), NameSet(getEntity(), MyData::classname, part),
+  //           MyData::classname, 0, Channel::Events),
+  // w_mytoken(getId(), NameSet(getEntity(), MyData2::classname, part),
+  //           MyData2::classname, "label", Channel::Continuous),
+  r_announce(getId(), NameSet(getEntity(), ReplicatorInfo::classname, part),
+             ReplicatorInfo::classname, 0, Channel::Events),
+  r_world(getId(), NameSet("world", BaseObjectMotion::classname, part),
+             BaseObjectMotion::classname, entry_any, Channel::AnyTimeAspect,
+	     Channel::ZeroOrMoreEntries),
+
+  // create a clock, if you need time based triggering
+  // instead of triggering on the incoming channels
+  myclock(),
+
+  // a callback object, pointing to the main calculation function
+  cb1(this, &_ThisModule_::doCalculation),
+  cb2(this, &_ThisModule_::doNotify),
+  // the module's main activity
+  do_calc(getId(), "ahow team status", &cb1, ps),
+  do_notify(getId(), "print notification", &cb2, ps)
+{
+  // connect the triggers for simulation
+  do_calc.setTrigger(myclock);
+  do_notify.setTrigger(r_announce /* fill in your triggering channels,
+                        or enter the clock here */);
+}
+
+bool MonitorTeams::complete()
+{
+  /* All your parameters have been set. You may do extended
+     initialisation here. Return false if something is wrong. */
+  do_notify.switchOn();
+  return true;
+}
+
+// destructor
+MonitorTeams::~MonitorTeams()
+{
+do_notify.switchOff();
+  //
+}
+
+// as an example, the setTimeSpec function
+bool MonitorTeams::setTimeSpec(const TimeSpec& ts)
+{
+  // a time span of 0 is not acceptable
+  if (ts.getValiditySpan() == 0) return false;
+
+  // specify the timespec to the activity
+  do_calc.setTimeSpec(ts);
+  // or do this with the clock if you have it (don't do both!)
+  myclock.changePeriodAndOffset(ts);
+
+  // do whatever else you need to process this in your model
+  // hint: ts.getDtInSeconds()
+
+  // return true if everything is acceptable
+  return true;
+}
+
+// the checkTiming function installs a check on the activity/activities
+// of the module
+bool MonitorTeams::checkTiming(const std::vector<int>& i)
+{
+  if (i.size() == 3) {
+    new TimingCheck(do_calc, i[0], i[1], i[2]);
+  }
+  else if (i.size() == 2) {
+    new TimingCheck(do_calc, i[0], i[1]);
+  }
+  else {
+    return false;
+  }
+  return true;
+}
+
+// tell DUECA you are prepared
+bool MonitorTeams::isPrepared()
+{
+  bool res = true;
+
+  // Example checking a token:
+  // CHECK_TOKEN(w_somedata);
+  CHECK_TOKEN(r_world);
+  CHECK_TOKEN(r_announce);
+
+  // Example checking anything
+  // CHECK_CONDITION(myfile.good());
+  // CHECK_CONDITION2(sometest, "some test failed");
+
+  // return result of checks
+  return res;
+}
+
+// start the module
+void MonitorTeams::startModule(const TimeSpec &time)
+{
+  do_calc.switchOn(time);
+}
+
+// stop the module
+void MonitorTeams::stopModule(const TimeSpec &time)
+{
+  do_calc.switchOff(time);
+}
+
+// this routine contains the main simulation process of your module. You
+// should read the input channels here, and calculate and write the
+// appropriate output
+void MonitorTeams::doCalculation(const TimeSpec& ts)
+{
+  // access the input
+  // example:
+  // try {
+  //   DataReader<MyData> u(r_mytoken, ts);
+  //   throttle = u.data().throttle;
+  //   de = u.data().de; ....
+  // }
+  // catch(Exception& e) {
+  //   // strange, there is no input. Should I try to continue or not?
+  // }
+  /* The above piece of code shows a block in which you try to catch
+     error conditions (exceptions) to handle the case in which the input
+     data is lost. This is not always necessary, if you normally do not
+     foresee such a condition, and you don t mind being stopped when
+     it happens, forget about the try/catch blocks. */
+
+  // do the simulation or other calculations, one step
+
+  // DUECA applications are data-driven. From the time a module is switched
+  // on, it should produce data, so that modules "downstream" are
+  // activated
+  // access your output channel(s)
+  // example
+  // DataWriter<MyData2> y(w_mytoken, ts);
+
+  // write the output into the output channel, using the data writer
+  // y.data().var1 = something; ...
+  unsigned ecount = 0;
+  while (r_world.haveEntry()) {
+	  ecount++;
+	  try {
+		  DataReader<BaseObjectMotion,MatchIntervalStartOrEarlier> om(r_world);
+		  std::cout << "Ufo " << r_world.getEntryLabel() << "now at "
+			  << om.data().xyz << std::endl;
+		  std::cout << "Current tick " << ts.getValidityStart()
+			  << ", data generated at "
+			  << om.timeSpec().getValidityStart() << std::endl;
+	  }
+	  catch (const NoDataAvailable& e) {
+		  std::cout << "Ufo " << r_world.getEntryLabel() << " no data" << std::endl;
+	  }
+	  r_world.selectNextEntry();
+  }
+  // this shows we looked
+  std::cout << "There were " << ecount << " entried" << std::endl;
+}
+
+void MonitorTeams::doNotify(const TimeSpec& ts)
+{
+	DataReader<ReplicatorInfo> ri(r_announce, ts);
+	cout << ri.data();
+}
+
+// Make a TypeCreator object for this module, the TypeCreator
+// will check in with the script code, and enable the
+// creation of modules of this type
+static TypeCreator<MonitorTeams> a(MonitorTeams::getMyParameterTable());
+
--- monitor-teams/MonitorTeams.hxx
+++ monitor-teams/MonitorTeams.hxx
@@ -1,114 +1,88 @@
 /* ------------------------------------------------------------------   */
-/*      item            : UFODynamics.hxx
+/*      item            : MonitorTeams.hxx
         made by         : ethung
-        from template   : DusimeModuleTemplate.hxx (2022.06)
-        date            : Mon Feb 27 14:11:24 2023
+        from template   : DuecaModuleTemplate.hxx (2022.06)
+        date            : Fri Dec 22 17:33:15 2023
         category        : header file
         description     :
-        changes         : Mon Feb 27 14:11:24 2023 first version
+        changes         : Fri Dec 22 17:33:15 2023 first version
         language        : C++
         copyright       : (c)
 */
 
-#ifndef UFODynamics_hxx
-#define UFODynamics_hxx
-
-// include headers for functions/classes you need in the module
-
-#include <array>
-#include <extra/RigidBody.hxx>
-#include <extra/integrate_rungekutta.hxx>
+#ifndef MonitorTeams_hxx
+#define MonitorTeams_hxx
 
 // include the dusime header
-#include <dusime.h>
+#include <dueca.h>
 USING_DUECA_NS;
 
 // This includes headers for the objects that are sent over the channels
 #include "comm-objects.h"
 
+// include headers for functions/classes you need in the module
+#include <dueca/inter/ReplicatorInfo.hxx>
 
-/** A module.
 
-    The instructions to create an module of this class from the Scheme
+
+/** A simulation module.
+
+    The instructions to create an module of this class from the start
     script are:
 
-    \verbinclude ufo-dynamics.scm
-*/
-class UFODynamics: public SimulationModule
+    \verbinclude monitor-teams.scm
+ */
+class MonitorTeams: public Module
 {
   /** self-define the module type, to ease writing the parameter table */
-  typedef UFODynamics _ThisModule_;
+  typedef MonitorTeams _ThisModule_;
 
 private: // simulation data
   // declare the data you need in your simulation
 
-    // Rigid body dynamics
-  RigidBody            body;
-
-  // Workspace for the integration
-  RungeKuttaWorkspace  ws;
-
-  // Time constant for following rotational inputs
-  double               tau_r;
-
-    // Time constant for following linear inputs
-  double               tau_v;
-
-
-private: // trim calculation data
-  // declare the trim calculation data needed for your simulation
-
-private: // snapshot data
-  // declare, if you need, the room for placing snapshot data
-
-    // temporary storage for the capturing the state
-  //std::array<12,double> snapcopy;
 private: // channel access
   // declare access tokens for all the channels you read and write
   // examples:
-  ChannelReadToken    r_controls;
-  ChannelWriteToken   w_egomotion;
-  ChannelWriteToken   hudw_token;
-  ChannelWriteToken   w_world;
-
+  // ChannelReadToken    r_mytoken;
+  ChannelReadToken    r_announce;
+  ChannelReadToken    r_world;
+  // ChannelWriteToken   w_mytoken;
 
 private: // activity allocation
   /** You might also need a clock. Don't mis-use this, because it is
       generally better to trigger on the incoming channels */
-  //PeriodicAlarm        myclock;
+  PeriodicAlarm        myclock;
 
   /** Callback object for simulation calculation. */
-  Callback<UFODynamics>  cb1;
+  Callback<MonitorTeams>  cb1;
+  Callback<MonitorTeams>  cb2;
 
   /** Activity for simulation calculation. */
   ActivityCallback      do_calc;
+  ActivityCallback      do_notify;
 
 public: // class name and trim/parameter tables
   /** Name of the module. */
   static const char* const           classname;
-
-  /** Return the initial condition table. */
-  static const IncoTable*            getMyIncoTable();
 
   /** Return the parameter table. */
   static const ParameterTable*       getMyParameterTable();
 
 public: // construction and further specification
   /** Constructor. Is normally called from scheme/the creation script. */
-  UFODynamics(Entity* e, const char* part, const PrioritySpec& ts);
+  MonitorTeams(Entity* e, const char* part, const PrioritySpec& ts);
 
   /** Continued construction. This is called after all script
       parameters have been read and filled in, according to the
       parameter table. Your running environment, e.g. for OpenGL
-      drawing, is also prepared. Any lengty initialisations (like
+      drawing, is also prepared. Any lengthy initialisations (like
       reading the 4 GB of wind tables) should be done here.
       Return false if something in the parameters is wrong (by
       the way, it would help if you printed what!) May be deleted. */
-
   bool complete();
 
   /** Destructor. */
-  ~UFODynamics();
+  ~MonitorTeams();
 
   // add here the member functions you want to be called with further
   // parameters. These are then also added in the parameter table
@@ -134,23 +108,8 @@ public: // member functions for cooperation with DUECA
 public: // the member functions that are called for activities
   /** the method that implements the main calculation. */
   void doCalculation(const TimeSpec& ts);
-
-public: // member functions for cooperation with DUSIME
-  /** For the Snapshot capability, fill the snapshot "snap" with the
-      data saved at a point in your simulation (if from_trim is false)
-      or with the state data calculated in the trim calculation (if
-      from_trim is true). */
-  void fillSnapshot(const TimeSpec& ts,
-                    Snapshot& snap, bool from_trim);
-
-  /** Restoring the state of the simulation from a snapshot. */
-  void loadSnapshot(const TimeSpec& t, const Snapshot& snap);
-
-  /** Perform a trim calculation. Should NOT use current state
-      uses event channels parallel to the stream data channels,
-      calculates, based on the event channel input, the steady state
-      output. */
-  void trimCalculation(const TimeSpec& ts, const TrimMode& mode);
+  /** print leaving or joining peer */
+  void doNotify(const TimeSpec& ts);
 };
 
 #endif
